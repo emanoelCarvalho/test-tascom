4dda76f085abe11c8aeb4b893ce42b16cedd37ec
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-04-03 12:37:05 -0300

    feat: add user creation endpoint to UsersController with API versioning

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './user.model';
 
-@Controller('users')
+@Controller('api/v1/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() createUserDto: any): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
 }
