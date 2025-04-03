a4aff602be310b906a023c2a12a666f3d9c00482
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-04-03 11:43:27 -0300

    :heavy_plus_sign: add Tags module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { TasksModule } from './tasks/tasks.module';
 import { DatabaseModule } from './config/database/database.module';
 import { EnvModule } from './config/env/env.module';
 import { TagsModule } from './tags/tags.module';
+import { TasgsController } from './tasgs/tasgs.controller';
 
 @Module({
   imports: [UsersModule, TasksModule, DatabaseModule, EnvModule, TagsModule],
+  controllers: [TasgsController],
 })
 export class AppModule {}
