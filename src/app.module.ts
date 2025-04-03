111ca1fa41bf1c5e8c7d64b48a02d8f495bdf5a7
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-04-03 10:58:38 -0300

    :wrench: configuration initial project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { DatabaseModule } from './database/database.module';
+import { EnvModule } from './env/env.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, TasksModule, DatabaseModule, EnvModule],
 })
 export class AppModule {}
